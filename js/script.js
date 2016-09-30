9bedb2cd3d12ab87d8583714187e6b6798593ead
Kalvyn Rasquinha <krasquin@adobe.com>
2016-09-30 00:27:22 -0500

    Playing around with APIs

--- js/script.js
+++ js/script.js
@@ -14,7 +14,36 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
+    var $street = $('#street').val();
+    var $city = $('#city').val();
+    var location = encodeURIComponent($street) + ', ' + encodeURIComponent($city);
+    
+    var imageUrl = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + location + '&heading=151.78&pitch=-0.76&key=' + GOOGLE_STREETVIEW_API_KEY;
 
+    $body.append('<img class="bgimg" src="' + imageUrl + '">');
+    
+    var placesUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + location + '&radius=500&type=food&key=' + GOOGLE_PLACES_API_KEY;
+    console.log(placesUrl);
+    
+    $.getJSON(placesUrl, function(data) {
+       console.log(data); 
+    });
+    
+    var wikipediaUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + encodeURIComponent($city) + '&format=json';
+    
+    var wikiRequestTimeout = setTimeout(function() {
+        $wikiElem.text("Failed to get wiki resources");
+    }, 8000);
+    
+    $.ajax(wikipediaUrl, {
+        dataType: "jsonp",
+        success: function(response) {
+            console.log(response);
+            
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
+    
     return false;
 };
 
